ca2c5d1bb6942e1d8fa7f32dca67ab1272883160
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-07-06 20:02:45 +0700

    KT

---
+++ KT59_TranThanhHai_59130561/src/kt59_tranthanhhai_59130561/KT59_TranThanhHai_59130561.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kt59_tranthanhhai_59130561;
+
+/**
+ *
+ * @author Admin
+ */
+public class KT59_TranThanhHai_59130561 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("kt59_tranthanhhai_59130561.KT59_TranThanhHai_59130561.main()");
+    }
+    
+}
